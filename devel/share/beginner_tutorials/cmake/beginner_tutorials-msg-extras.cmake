6f61b925f1dc63e08c8150a05706f1c0afdfe5fd
zdevt <zhangtao.hawk@qq.com>
2018-05-23 15:35:29 +0800

    rm devel

--- devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/dev/ros_catkin_ws_show_me_the_code/src/beginner_tutorials/msg/Num.msg")
-set(beginner_tutorials_SERVICE_FILES "/home/dev/ros_catkin_ws_show_me_the_code/src/beginner_tutorials/srv/AddTwoInts.srv")
--- devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(beginner_tutorials_MSG_INCLUDE_DIRS "/home/dev/ros_catkin_ws_show_me_the_code/src/beginner_tutorials/msg")
-set(beginner_tutorials_MSG_DEPENDENCIES std_msgs)
