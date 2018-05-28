9b2ee9df8d0bc8adbb791ab93c7541d530c85914
zdevt <zhangtao.hawk@qq.com>
2018-05-28 10:16:00 +0800

    add ros shutdown hook test

--- src/zt/scripts/talker.py
+++ src/zt/scripts/talker.py
@@ -1,12 +1,12 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*- 
 #       FileName:  talker.py
 #
 #    Description:
 #
 #        Version:  1.0
 #        Created:  2018-05-24 10:12:40
-#  Last Modified:  2018-05-24 10:23:03
+#  Last Modified:  2018-05-28 10:15:28
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -17,21 +17,27 @@ import rospy
 from std_msgs.msg import String
 
 
+def myhook():
+    print 'is_shutdown time %s' % rospy.get_time()
+
+
 def talker():
-    pub = rospy.Publisher('chat2',String,queue_size=10)
-    rospy.init_node('talker',anonymous=True)
+    pub = rospy.Publisher('chat2', String, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rospy.on_shutdown(myhook)
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
-        str = "test %s" % rospy.get_name()
-        rospy.loginfo(str)
+        str = 'test %s' % rospy.get_name()
+
+        # rospy.loginfo(str)
+
         pub.publish(str)
         rate.sleep()
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     try:
         talker()
     except rospy.ROSInterruptException:
         pass
-
-
 
