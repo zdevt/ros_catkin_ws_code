023096994c0946c3558d43833df4b0738abc88e5
zdevt <zhangtao.hawk@qq.com>
2018-06-01 09:13:49 +0800

    recover

--- src/zt/scripts/video_listener.py
+++ src/zt/scripts/video_listener.py
@@ -7,7 +7,7 @@
 #
 #        Version:  1.0
 #        Created:  2018-05-26 13:19:37
-#  Last Modified:  2018-06-01 09:01:50
+#  Last Modified:  2018-06-01 09:12:51
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -20,20 +20,22 @@ import rospy
 import numpy as np
 
 from cv_bridge import CvBridge, CvBridgeError
-# from sensor_msgs.msg import Image
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
+
+# from sensor_msgs.msg import CompressedImage
 
 
 def callback(data):
-    #frame = CvBridge().imgmsg_to_cv2(data, 'bgr8')
-    frame = CvBridge().compressed_imgmsg_to_cv2(data, 'bgr8')
+    frame = CvBridge().imgmsg_to_cv2(data, 'bgr8')
+    # frame = CvBridge().compressed_imgmsg_to_cv2(data, 'bgr8')
     cv2.imshow('frame', frame)
     cv2.waitKey(1)
 
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-    sub = rospy.Subscriber('video', CompressedImage, callback)
+    sub = rospy.Subscriber('video', Image, callback)
+    # sub = rospy.Subscriber('video', CompressedImage, callback)
     rospy.spin()
 
 
--- src/zt/scripts/video_talker.py
+++ src/zt/scripts/video_talker.py
@@ -7,7 +7,7 @@
 #
 #        Version:  1.0
 #        Created:  2018-05-26 11:15:48
-#  Last Modified:  2018-06-01 09:01:35
+#  Last Modified:  2018-06-01 09:13:28
 #       Revision:  none
 #       Compiler:  gcc
 #
@@ -20,21 +20,23 @@ import rospy
 import numpy as np
 
 from cv_bridge import CvBridge, CvBridgeError
-#from sensor_msgs.msg import Image
-from sensor_msgs.msg import CompressedImage
+from sensor_msgs.msg import Image
+
+# from sensor_msgs.msg import CompressedImage
 
 
 def GetImgAndPub():
     rospy.init_node('video_talker', anonymous=True)
-    pub = rospy.Publisher('video', CompressedImage, queue_size=10)
+    pub = rospy.Publisher('video', Image, queue_size=10)
+    # pub = rospy.Publisher('video', CompressedImage, queue_size=10)
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FPS, 10)
 
     while cap.isOpened() and not rospy.is_shutdown():
         (_, frame) = cap.read()
         frame = imutils.resize(frame, width=300)
-        #data = CvBridge().cv2_to_imgmsg(frame, 'bgr8')
-        data = CvBridge().cv2_to_compressed_imgmsg(frame, 'bgr8')
+        data = CvBridge().cv2_to_imgmsg(frame, 'bgr8')
+        # data = CvBridge().cv2_to_compressed_imgmsg(frame, 'bgr8')
         pub.publish(data)
         # cv2.imshow('frame', frame)
         # cv2.waitKey(1)
@@ -45,4 +47,3 @@ def GetImgAndPub():
 
 if __name__ == '__main__':
     GetImgAndPub()
-
